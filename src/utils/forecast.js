88eb707d468a1ea7e052cd7627afd81a505055ae
Mancenido <janine.j.b.mancenido@accenture.com>
2020-09-18 22:26:11 +0800

    Added feelslike data.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(body.error, undefined)
         } else {
             callback( undefined,
-                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + ' chance of rain.'
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees. There is a ' + body.current.precip + '% chance of rain.'
             )
         }
     })
